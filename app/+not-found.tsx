2ac7c0f88396a2a8cf37e36d92715d758e81ccb4
nyambogahezron <hezzyn1@gmail.com>
2024-09-28 23:23:27 +0300

    initial setup

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,17 +1,16 @@
 import { Link, Stack } from 'expo-router';
 import { StyleSheet } from 'react-native';
 
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { ThemedText, ThemedView } from '@/components/Themed';
 
 export default function NotFoundScreen() {
   return (
     <>
       <Stack.Screen options={{ title: 'Oops!' }} />
       <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen doesn't exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
+        <ThemedText type='title'>This screen doesn't exist.</ThemedText>
+        <Link href='/' style={styles.link}>
+          <ThemedText type='link'>Go to home screen!</ThemedText>
         </Link>
       </ThemedView>
     </>
---
+++ components/Themed/index.ts
@@ -0,0 +1,4 @@
+import { ThemedText } from './ThemedText';
+import { ThemedView } from './ThemedView';
+
+export { ThemedText, ThemedView };
